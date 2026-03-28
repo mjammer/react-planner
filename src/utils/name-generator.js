fd70f9645c6cef15457fdf6714cac5ed09d99fdd
mijia <mijia2013@gmail.com>
2026-03-28 10:00:48 +0800

    优化名称

--- src/utils/name-generator.js
+++ src/utils/name-generator.js
@@ -1,5 +1,11 @@
 export default class NameGenerator {
   static generateName( prototype, type ) {
-    return type.substr(0, 1).toUpperCase() + type.substr(1);
+    const normalizedType = typeof type === 'string' && type.trim()
+      ? type.trim()
+      : typeof prototype === 'string' && prototype.trim()
+        ? prototype.trim()
+        : 'element';
+
+    return normalizedType.substr(0, 1).toUpperCase() + normalizedType.substr(1);
   }
 }
